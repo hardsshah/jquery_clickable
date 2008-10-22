a31416cb5f41118b4dcb57174a848df5c75e7c60
unknown <matt@mcurry.net>
2008-10-21 23:41:15 -0400

    update header

--- jquery.clickable.js
+++ jquery.clickable.js
@@ -1,23 +1,30 @@
+/*
+ * jQuery Clickable Plugin
+ * Copyright (c) 2008 Matt Curry
+ * www.PseudoCoder.com
+ * http://github.com/mcurry/jquery/tree/master/clickable
+ * http://www.pseudocoder.com/archives/2008/10/21/clickable-box-with-jquery-part-2-the-plugin/
+ *
+ * @author      Matt Curry <matt@pseudocoder.com>
+ * @license     MIT
+ *
+ */
+ 
 (function($) {
-  //
-  // plugin definition
-  //
   $.fn.clickable = function(options) {
     var opts = $.extend({}, $.fn.clickable.defaults, options);
 	
 		return this.each(function() {
       $(this).bind("click", function() {
-        window.location = $(this).find("a:first").attr("href");
+        window.location = $(this).find(opts.link).attr("href");
       });
       
       $(this).hover(opts.hover, opts.unhover);
 		});
   };
   
-  //
-  // plugin defaults
-  //
   $.fn.clickable.defaults = {
+    link: "a:first",
 		hover: function() {},
 		unhover: function() {}
   };
